8daac5aa20b59998e5b1fa94d7f53d81ad6958e2
sudiptoroy12 <sudipto1294@gmail.com>
2026-07-12 23:17:20 +0600

    home page route complete

--- index.js
+++ index.js
@@ -3,8 +3,31 @@ const http = require("http")
 
 const server = http.createServer((req,res)=>{
 
-    res.write("server start")
-    res.end()
+    if(req.url === "/" || req.url === "/home"){
+        res.writeHead(200, {"content-type" : "text/html"})
+        res.write(`
+            <html>
+              <head><title>Home Page</title></head>
+              <body>
+                   <nav>
+                       <a href="/">Home</a> | 
+                       <a href="/about">About</a> | 
+                       <a href="/contact">Contact</a>
+                    </nav>
+                    <h1>Welcome to the Home Page</h1>
+                    <p>This is the main landing page of our Node.js HTTP server.</p>
+                </body>
+             
+            </html>`)
+      
+    }
+
+    else{
+         res.write("server start")
+         res.end()
+    }
+
+   
 
 })
 
