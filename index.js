6c1854630a2e8c35b53f51b4b832a0713fec17ec
sudiptoroy12 <sudipto1294@gmail.com>
2026-07-12 23:26:05 +0600

    about page route create

--- index.js
+++ index.js
@@ -14,11 +14,34 @@ const server = http.createServer((req,res)=>{
                        <a href="/about">About</a> | 
                        <a href="/contact">Contact</a>
                     </nav>
+                    <hr>
                     <h1>Welcome to the Home Page</h1>
                     <p>This is the main landing page of our Node.js HTTP server.</p>
                 </body>
              
             </html>`)
+        res.end()
+      
+    }
+    else if(req.url === "/about"){
+        res.writeHead(200, {"content-type" : "text/html"})
+        res.write(`
+            <html>
+              <head><title>About Page</title></head>
+              <body>
+                   <nav>
+                       <a href="/">Home</a> | 
+                       <a href="/about">About</a> | 
+                       <a href="/contact">Contact</a>
+                    </nav>
+                    <hr>
+                    <h1>About Us</h1>
+                    <p>Learn more about our journey, vision, and the team behind this application.</p>
+                    
+                </body>
+             
+            </html>`)
+        res.end()
       
     }
 
